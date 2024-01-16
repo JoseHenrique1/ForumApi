df0d9508ea9fb87139865c6b07d1cfc3777a6c96
Jose Henrique <jh4832184@gmail.com>
2024-01-16 14:31:08 -0300

    env: add variables env host and dialect

--- database/db.js
+++ database/db.js
@@ -3,10 +3,12 @@ import { Sequelize } from "sequelize";
 let db_name = process.env.DB_NAME;
 let db_user = process.env.DB_USER;
 let db_password = process.env.DB_PASSWORD;
+let db_host = process.env.DB_HOST;
+let db_dialect = process.env.DB_DIALECT;
 
 const sequelize = new Sequelize(db_name, db_user, db_password, {
-    host: 'localhost',
-    dialect: 'mysql'
+    host: db_host,
+    dialect: db_dialect
 });
 
 export default sequelize;
