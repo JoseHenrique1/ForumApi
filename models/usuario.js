da74277b46f8578949d504d02a574805b3bf872e
Jose Henrique <jh4832184@gmail.com>
2024-01-29 13:59:15 -0300

    fix: add restriction unique email and max length

--- models/usuario.js
+++ models/usuario.js
@@ -4,8 +4,9 @@ import { DataTypes } from "sequelize";
 const Usuario = await sequelize.define("usuario", {
     nome: DataTypes.TEXT,
     email: {
-      type: DataTypes.TEXT,
+      type: DataTypes.STRING(100),
       allowNull: false,
+      unique: true
     },
     senha: {
       type: DataTypes.TEXT,
