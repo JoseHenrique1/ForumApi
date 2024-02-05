02144ae78e1c1a18ac9b22e8405637b20ca37120
Jose Henrique <jh4832184@gmail.com>
2024-02-05 00:19:53 -0300

    env: add port db

--- database/db.js
+++ database/db.js
@@ -5,10 +5,12 @@ let db_user = process.env.DB_USER;
 let db_password = process.env.DB_PASSWORD;
 let db_host = process.env.DB_HOST;
 let db_dialect = process.env.DB_DIALECT;
+let db_port = process.env.DB_PORT 
 
 const sequelize = new Sequelize(db_name, db_user, db_password, {
     host: db_host,
-    dialect: db_dialect
+    dialect: db_dialect,
+    port: db_port
 });
 
 export default sequelize;
